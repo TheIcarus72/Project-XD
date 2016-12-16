abc6d5bc2ce189e56342fbfdc93d89770c052abc
Dave Bechtum <dbechtum@gmail.com>
2016-12-16 14:42:25 +0100

    Added Experiments/menu3D

---
+++ Experiments/menu3D/Assets/Scripts/RotatePlanet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatePlanet : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0f *Time.deltaTime, 1f *Time.deltaTime * speed, 0f *Time.deltaTime, Space.Self);
+	}
+}
